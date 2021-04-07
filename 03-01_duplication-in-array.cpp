1bb687a1a88c0cbff24ebd8c0cc25169fecd50d3
allancodeman <allancodeman@163.com>
2021-04-08 00:47:39 +0800

    add testing case for 03-01

--- 03-01_duplication-in-array.cpp
+++ 03-01_duplication-in-array.cpp
@@ -5,6 +5,8 @@
  * 在一个长度为n的数组里，所有的数字都在0～n-1的范围内。数组中的某些数字是重复的，但不知道有几个数字重复了，也不知道那些数字重复了几次。
  * 请找出数组中任意一个重复的数字。例如，如果输入长度为7的数组[2,3,1,0,2,5,3]，那么对应的输出的重复的数字是2或者3。
  */
+#include <iostream>
+
 bool find_duplicate(int numbers[], int length, int* duplication)
 {
     // 判断数组是否为空，数组长度是否大于0
@@ -37,3 +39,16 @@ bool find_duplicate(int numbers[], int length, int* duplication)
     return false;
 }
 
+int main()
+{
+    int numbers[] = {2,3,1,0,2,5,3};
+    int length = 7;
+    int* duplication;
+    bool find = find_duplicate(numbers, length, duplication);
+    if (find)
+        std::cout << duplication << std::endl;
+    else
+        std::cout << "Not find!" << std::endl;
+
+    return 0;
+}
